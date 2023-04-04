9eb617d117fe8b5e50a657201248abcb8b204e90
Moises Alvarenga <moises.alvarenga@outlook.com>
2023-04-03 23:28:45 -0300

    Colocando imagem default de lista vazia

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -11,57 +11,80 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 600,
-      child: ListView.builder(
-        itemCount: transactions.length,
-        itemBuilder: (ctx, index) {
-          final tr = transactions[index];
-          return Card(
-            child: Row(
-              children: <Widget>[
+      child: transactions.isEmpty
+          ? Column(
+              children: [
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "Nenhuma tranzação cadastrada!",
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
                 Container(
-                  margin: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.purple,
-                      width: 2,
-                    ),
+                  height: 400,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
                   ),
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    'R\$ ${tr.value.toStringAsFixed(2)}',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.purple,
-                    ),
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Container(
-                      child: Text(
-                        // style: TextStyle(
-                        //   fontWeight: FontWeight.bold,
-                        //   fontSize: 16,
-                        // ),
-                        tr.title,
-                        style: Theme.of(context).textTheme.headline6,
-                      ),
-                    ),
-                    Container(
-                      child: Text(
-                        DateFormat('d MMM y').format(tr.date),
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    )
-                  ],
-                ),
+                )
               ],
+            )
+          : ListView.builder(
+              itemCount: transactions.length,
+              itemBuilder: (ctx, index) {
+                final tr = transactions[index];
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin:
+                            EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.purple,
+                            width: 2,
+                          ),
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          'R\$ ${tr.value.toStringAsFixed(2)}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Container(
+                            child: Text(
+                              // style: TextStyle(
+                              //   fontWeight: FontWeight.bold,
+                              //   fontSize: 16,
+                              // ),
+                              tr.title,
+                              style: Theme.of(context).textTheme.headline6,
+                            ),
+                          ),
+                          Container(
+                            child: Text(
+                              DateFormat('d MMM y').format(tr.date),
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
             ),
-          );
-        },
-      ),
     );
   }
 }
