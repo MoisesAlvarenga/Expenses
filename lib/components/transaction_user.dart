894f82d677a2ec8fb8dd97af64e0385c9e5f81ae
Moises Alvarenga <moises.alvarenga@outlook.com>
2023-04-03 22:09:12 -0300

    Adicionando tema personalizado

--- lib/components/transaction_user.dart
+++ lib/components/transaction_user.dart
@@ -1,19 +1,19 @@
-import 'dart:math';
+// import 'dart:math';
 
-import 'package:expenses/components/transaction_form.dart';
-import 'package:expenses/components/transaction_list.dart';
-import 'package:flutter/material.dart';
+// import 'package:expenses/components/transaction_form.dart';
+// import 'package:expenses/components/transaction_list.dart';
+// import 'package:flutter/material.dart';
 
-import '../models/transaction.dart';
+// import '../models/transaction.dart';
 
-class TransactionUser extends StatefulWidget {
-  @override
-  _TransactionUserState createState() => _TransactionUserState();
-}
+// class TransactionUser extends StatefulWidget {
+//   @override
+//   _TransactionUserState createState() => _TransactionUserState();
+// }
 
-class _TransactionUserState extends State<TransactionUser> {
-  @override
-  Widget build(BuildContext context) {
-    return 
-  }
-}
+// class _TransactionUserState extends State<TransactionUser> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return 
+//   }
+// }
